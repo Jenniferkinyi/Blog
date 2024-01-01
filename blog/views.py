5349ccfe24ad62c6b7887cffe7115da9de1f1c6a
Jennifer A <95017424+Jenniferkinyi@users.noreply.github.com>
2024-01-01 17:28:25 +0300

    first commit

---
+++ blog/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 12,2023'
+    },
+    {
+        'author': 'Adrian',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 13,2023'
+    }
+
+]
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
+
